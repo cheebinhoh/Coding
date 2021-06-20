6cd4e70091041f8a5c4bebd2daab6b40d03f929d
Chee Bin HOH <hohcheebin@gmail.com>
2021-06-20 12:45:00 -0400

    Build binary tree from inorder and postorder list

--- treebuild.c
+++ treebuild.c
@@ -1,6 +1,6 @@
 /* Copyright © 2021 Chee Bin HOH. All rights reserved.
  *
- * Depth first search, preorder, inorder and postorder, both recursive and iterative way
+ * Build binary tree from inorder and postorder list.
  */
 
 #include <stdio.h>
