ab843445f90000e41332cbfcd6dff9e11eaa77b3
Chee Bin HOH <hohcheebin@gmail.com>
2021-06-19 20:16:00 -0400

    sysmetric check

---
+++ treesysmetriccheck.c
@@ -0,0 +1,213 @@
+/* Copyright © 2021 Chee Bin HOH. All rights reserved.
+ *
+ * Depth first search, preorder, inorder and postorder, both recursive and iterative way
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Definition for a binary tree node.
+ */
+struct TreeNode {
+
+     int val;
+     struct TreeNode *left;
+     struct TreeNode *right;
+};
+
+void linearTree(struct TreeNode  *root, 
+                int               left,
+                struct TreeNode **list,
+                int              *listCnt)
+{
+    if ( NULL == root )
+    {
+        list[*listCnt] = NULL;
+        *listCnt = *listCnt + 1;
+    }
+    else 
+    {
+        list[*listCnt] = root;
+        *listCnt = *listCnt + 1;
+  
+        if ( left ) 
+        {
+          linearTree(root->left, 
+                     left,
+                     list,
+                     listCnt);
+
+          linearTree(root->right, 
+                     left,
+                     list,
+                     listCnt);
+        }
+        else
+        {
+          linearTree(root->right,
+                     left,
+                     list,
+                     listCnt);
+          
+          linearTree(root->left,
+                     left,
+                     list,
+                     listCnt);
+        }
+    }
+}
+
+int treeIsSysmetric(struct TreeNode *root)
+{
+    if ( NULL == root )
+    {
+        return 1;
+    }
+    else 
+    {
+        struct TreeNode *leftList[100];
+        struct TreeNode *rightList[100];
+        int              leftListCnt = 0;
+        int              rightListCnt = 0; 
+        int              i;
+
+        linearTree(root, 1, leftList, &leftListCnt);
+        linearTree(root, 0, rightList, &rightListCnt);
+
+        printf("left  = " );
+        for ( i = 0; i < leftListCnt; i++ )
+        {
+            struct TreeNode *node;
+
+            node = leftList[i];
+            if ( NULL == node )
+            {
+                printf("-%s", i + 1 >= leftListCnt ? "" : ", "); 
+            }
+            else
+            {
+                printf("%d, ", node->val);
+            }
+        }
+
+        printf("\n");
+
+        printf("right = ");
+        for ( i = 0; i < rightListCnt; i++ )
+        {
+            struct TreeNode *node;
+
+            node = rightList[i];
+            if ( NULL == node )
+            {
+                printf("-%s", i + 1 >= rightListCnt ? "" : ", ");
+            }
+            else
+            {
+                printf("%d, ", node->val);
+            }
+        }
+        
+        printf("\n");
+
+        if ( leftListCnt != rightListCnt )
+        {
+            return 0;
+        }
+        else
+        {
+            for ( i = 0; i < leftListCnt; i++ )
+            {
+                if ( NULL != leftList[i]
+                     && NULL != rightList[i] )
+                {
+                    if ( leftList[i]->val != rightList[i]->val )
+                    {
+                        return 0;
+                    }   
+                }
+                else if ( leftList[i] != rightList[i] ) 
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
+
+/*
+ *               0
+ *               |
+ *      +--------+---------+
+ *      1                  1
+ *      |                  |
+ * +---------+        +---------+
+ * 2         3        3         2
+ *
+ */
+int main(int argc, char * argv[])
+{
+    struct TreeNode *root = NULL;
+    struct TreeNode *other = NULL;
+
+    root = malloc(sizeof( struct TreeNode ));
+    root->val = 0;
+    root->left = root->right = NULL;
+
+    other = malloc(sizeof( struct TreeNode ));
+    other->val = 1;
+    other->left = other->right = NULL;
+   
+    root->left = other;
+ 
+  
+    other = malloc(sizeof( struct TreeNode ));
+    other->val = 1;
+    other->left = other->right = NULL;
+    
+    root->right = other;
+
+
+    other = malloc(sizeof( struct TreeNode ));
+    other->val = 2;
+    other->left = other->right = NULL;
+
+    root->right->right = other;
+
+
+    other = malloc(sizeof( struct TreeNode ));
+    other->val = 2;
+    other->left = other->right = NULL;
+
+    root->left->left = other;
+
+
+    other = malloc(sizeof( struct TreeNode ));
+    other->val = 3;
+    other->left = other->right = NULL;
+
+    root->left->right = other;
+
+
+    other = malloc(sizeof( struct TreeNode ));
+    other->val = 3;
+    other->left = other->right = NULL;
+
+    root->right->left = other;
+
+    printf("Is sysmetric = %d\n", treeIsSysmetric(root));
+    printf("\n");
+
+    root->right->left = NULL;
+    
+    printf("Is sysmetric (after setting root->right->left to NULL) = %d\n", treeIsSysmetric(root));
+
+    // I do not care about freeing malloced memory, OS will take care of freeing heap that is part of process for
+    // this one off program.
+
+    return 0;
+ }
+
