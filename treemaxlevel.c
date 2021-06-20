4f964894958fd2958a9fa07bea14edd89466ba12
Chee Bin HOH <hohcheebin@gmail.com>
2021-06-20 12:45:39 -0400

    Find the tree maximum level

--- treemaxlevel.c
+++ treemaxlevel.c
@@ -1,6 +1,6 @@
 /* Copyright © 2021 Chee Bin HOH. All rights reserved.
  *
- * Depth first search, preorder, inorder and postorder, both recursive and iterative way
+ * Find the tree maximum level.
  */
 
 #include <stdio.h>
