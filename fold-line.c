4deea7ce65cb9fa5a752535ec17c0c610ac4f87c
Chee Bin HOH <hohcheebin@gmail.com>
2021-07-11 17:13:56 -0400

    It splits/folds a long line into multiple line

--- fold-line.c
+++ fold-line.c
@@ -53,57 +53,57 @@ int main(int argc, char *argv[])
     {
         if ( bufferUsed < maxline )
         {
-	    buffer[bufferUsed++] = c;
+            buffer[bufferUsed++] = c;
             buffer[bufferUsed] = '\0';
-	}
+        }
         else
         {
             char *cptr = &buffer[bufferUsed - 1];
 
-            while ( ' ' != *cptr 
+            while ( ' ' != *cptr
                     && cptr != buffer )
             {
-	        cptr--;
-	    }
+                cptr--;
+            }
 
             // the line input so far has no space, so we print everything out.
             if ( buffer == cptr )
             {
-		for ( int n = 0; n < bufferUsed; n++ )
-		{
+                for ( int n = 0; n < bufferUsed; n++ )
+                {
                     putchar(buffer[n]);
-		}
+                }
 
                 bufferUsed = 0;
                 buffer[bufferUsed] = '\0';
             }
             else // the line input has space, so we print the line until space, and then continue to build up line.
             {
-		char *iter = buffer;
+                char *iter = buffer;
 
-		while ( iter != cptr )
-		{
-		    putchar(*iter);
-		    iter++;
-		}
-
-  		iter = buffer;
-		cptr++;
-		while ( cptr != &buffer[bufferUsed] )
-		{
-		    *iter = *cptr;
+                while ( iter != cptr )
+                {
+                    putchar(*iter);
                     iter++;
-	            cptr++;
-		}
+                }
 
-		*iter = '\0';
-		bufferUsed = iter - buffer + sizeof(buffer[0]);
- 
-		putchar('\n');
-            } 
+                  iter = buffer;
+                cptr++;
+                while ( cptr != &buffer[bufferUsed] )
+                {
+                    *iter = *cptr;
+                    iter++;
+                    cptr++;
+                }
 
-	    buffer[bufferUsed++] = c;
-	    buffer[bufferUsed] = '\0';     
+                *iter = '\0';
+                bufferUsed = iter - buffer + sizeof(buffer[0]);
+
+                putchar('\n');
+            }
+
+            buffer[bufferUsed++] = c;
+            buffer[bufferUsed] = '\0';
         }
     }
 
@@ -111,11 +111,11 @@ int main(int argc, char *argv[])
     {
         int n;
 
-	while ( n < bufferUsed )
-	{
-	    putchar(buffer[n]);
-	    n++;
-	}
+        while ( n < bufferUsed )
+        {
+            putchar(buffer[n]);
+            n++;
+        }
     }
 
     return 0;
