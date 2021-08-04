c9603f22c961982ea8c2b781424ff7677ec42e1f
Chee Bin HOH <hohcheebin@gmail.com>
2021-08-04 08:54:27 -0400

    Transform one string to another using minimum number of given operation

--- transStr2Str.c
+++ transStr2Str.c
@@ -7,6 +7,12 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int   debug = 0;
+char *programName = NULL;
 
 
 int distanceFromPivot(char string[], char pivot)
@@ -99,6 +105,11 @@ int transform(char source[], char target[])
                j++;
            }
 
+           if ( debug )
+           {
+               printf("---- Move %d, pick %c and insert at the front, %s => ", move + 1, source[pivot], source);
+           }
+
            j   = pivot;
            tmp = source[j];
            while ( j > 0 )
@@ -110,6 +121,12 @@ int transform(char source[], char target[])
            source[0] = tmp;
            i = 0;
 
+           if ( debug )
+           {
+               printf("%s\n", source);
+           }
+
+
            move++;
         } // if ( source[i] == target[i] ) ... else
     } //  while ( source[i] != '\0' )
@@ -117,18 +134,48 @@ int transform(char source[], char target[])
     return move;
 }
 
+void printHelp(void)
+{
+     fprintf(stderr, "%s [-d] [-h]\n", programName);
+     fprintf(stderr, "\n");
+     fprintf(stderr, " -d : debug mode to demonstate the move\n");
+     fprintf(stderr, " -h : print this help message\n");
+}
+
+
 int main(int argc, char *argv[])
 {
-    char str1[]   = "EACBD";
-    char str2[]   = "CABED";
-    char str3[]   = "EACBD";
-    char str4[]   = "ABECD";
-    char str5[]   = "ACEBD";
-    char str6[]   = "ABCDE";
-    char str7[]   = "DEABC";
-    char target[] = "EABCD";
+    char str1[]    = "EACBD";
+    char str2[]    = "CABED";
+    char str3[]    = "EACBD";
+    char str4[]    = "ABECD";
+    char str5[]    = "ACEBD";
+    char str6[]    = "ABCDE";
+    char str7[]    = "DEABC";
+    char target[]  = "EABCD";
+    char str8[]    = "OHH";
+    char str9[]    = "HHO";
+    char target2[] = "HOH";
     int  moveCnt = 0;
+    int  c;
 
+
+    programName = argv[0];
+
+    while ( ( c = getopt(argc, argv, "dh") ) != -1 )
+    {
+        switch ( c )
+        {
+            case 'd':
+                debug = 1;
+                break;
+
+            case '?':
+            case 'h':
+                printHelp();
+                exit(1);
+        }
+    }
 
     printf("target string = %s\n\n", target);
 
@@ -136,33 +183,43 @@ int main(int argc, char *argv[])
     // because comma separator on function arguments are not sequencing point, there
     // is no guarantee that 1st str1 is passed to printf before it is transformed by nested method transform
 
-    printf("string1 = %s\n", str1);
+    printf("Transofmr string1 = %s to %s\n", str1, target);
     moveCnt = transform(str1, target);
-    printf("after %d move, string1 = %s\n", moveCnt, str1);
+    printf("after %d move, string1 = %s\n\n", moveCnt, str1);
 
-    printf("string2 = %s\n", str2);
+    printf("Transofmr string2 = %s to %s\n", str2, target);
     moveCnt = transform(str2, target);
-    printf("after %d move, string2 = %s\n", moveCnt, str2);
+    printf("after %d move, string2 = %s\n\n", moveCnt, str2);
 
-    printf("string3 = %s\n", str3);
+    printf("Transofmr string3 = %s to %s\n", str3, target);
     moveCnt = transform(str3, target);
-    printf("after %d move, string3 = %s\n", moveCnt, str3);
+    printf("after %d move, string3 = %s\n\n", moveCnt, str3);
 
-    printf("string4 = %s\n", str4);
+    printf("Transofmr string4 = %s to %s\n", str4, target);
     moveCnt = transform(str4, target);
-    printf("after %d move, string4 = %s\n", moveCnt, str4);
+    printf("after %d move, string4 = %s\n\n", moveCnt, str4);
 
-    printf("string5 = %s\n", str5);
+    printf("Transofmr string5 = %s to %s\n", str5, target);
     moveCnt = transform(str5, target);
-    printf("after %d move, string5 = %s\n", moveCnt, str5);
+    printf("after %d move, string5 = %s\n\n", moveCnt, str5);
 
-    printf("string6 = %s\n", str6);
+    printf("Transofmr string6 = %s to %s\n", str6, target);
     moveCnt = transform(str6, target);
-    printf("after %d move, string6 = %s\n", moveCnt, str6);
+    printf("after %d move, string6 = %s\n\n", moveCnt, str6);
 
-    printf("string7 = %s\n", str7);
+    printf("Transofmr string7 = %s to %s\n", str7, target);
     moveCnt = transform(str7, target);
-    printf("after %d move, string7 = %s\n", moveCnt, str7);
+    printf("after %d move, string7 = %s\n\n", moveCnt, str7);
+
+    printf("target string = %s\n\n", target2);
+
+    printf("Transofmr string8 = %s to %s\n", str8, target2);
+    moveCnt = transform(str8, target2);
+    printf("after %d move, string8 = %s\n\n", moveCnt, str8);
+
+    printf("Transofmr string9 = %s to %s\n", str9, target2);
+    moveCnt = transform(str9, target2);
+    printf("after %d move, string9 = %s\n\n", moveCnt, str9);
 
     return 0;
 }
