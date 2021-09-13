6d2d232204a60373071caa55607acaf7b9cc75b4
Chee Bin HOH <34779770+cheebinhoh@users.noreply.github.com>
2021-09-12 23:11:06 -0400

    Add delTreeNode and delTreeNodeAndRebalanceTree

--- btree.h
+++ btree.h
@@ -16,9 +16,7 @@ typedef void (bTreeTraversalCallback)(struct TreeNode *node, int pos, void *data
 struct TreeNode * addTreeNode(struct TreeNode *root, int val);
 struct TreeNode * findTreeNode(struct TreeNode *root, int val);
 struct TreeNode * treeRebalance(struct TreeNode *root);
-
 struct TreeNode * delTreeNode(struct TreeNode *root, int val);
-void findTreeNodeAndParent(struct TreeNode *root, int val, struct TreeNode **node, struct TreeNode ***parent);
 
 void printTreeNodeInOrder(struct TreeNode *root);
 void printTreeNodeInTreeTopology(struct TreeNode *root);
