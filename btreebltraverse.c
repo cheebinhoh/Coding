9ae8c40815626e30527299e71c305d9e3fdeaff7
Chee Bin HOH <34779770+cheebinhoh@users.noreply.github.com>
2021-09-22 19:40:28 -0400

    Breadth first search, both recursive and iterative way

--- btreebltraverse.c
+++ btreebltraverse.c
@@ -153,21 +153,6 @@ void breadthLevelTraversal(struct TreeNode *root)
 }
 
 
-/*
- *               0
- *               |
- *      +--------+---------+
- *      1                  2
- *      |                  |
- * +----+----+       //----+----+
- * 3         4                  5
- *                              |
- *                    +---------+---------+
- *                    6                   8
- *                    |                   |
- *              //----+----+          //--+--//
- *                         7
- */
 int main(int argc, char * argv[])
 {
     struct TreeNode *root  = NULL;
