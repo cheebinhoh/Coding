293d3defae41aa5efc58159283b2caa48ba9ac70
CheeBinHoh <>
2021-10-10 16:27:52 -0400

    system programming practicing

---
+++ Linux/listdir.c
@@ -0,0 +1,42 @@
+/* Copyright © 2021 Chee Bin HOH. All rights reserved.
+ *
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <dirent.h>
+
+char *programName = NULL;
+
+int main(int argc, char *argv[])
+{
+    DIR           *dp;
+    struct dirent *dirp;
+
+
+    programName = basename(argv[0]);
+    
+    if ( argc != 2 )
+    {
+        fprintf(stderr, "Usage: %s directory\n", programName);
+        exit(1);
+    }
+ 
+    if ( ( dp = opendir(argv[1] ) ) == NULL )
+    {
+        fprintf(stderr, "%s: %s\n", programName,
+                strerror(errno));
+        exit(2);
+    }
+
+    while ( ( dirp = readdir(dp) ) != NULL )
+        printf("%s\n", dirp->d_name);
+
+    closedir(dp);
+
+    return 0;
+}
+
