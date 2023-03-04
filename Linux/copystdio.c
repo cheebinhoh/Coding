b1a61b1f91945f356a79492988893dbd85760e6d
hohcheebin <hohcheebin@gmail.com>
2023-03-04 18:54:22 -0500

    no comment

--- Linux/copystdio.c
+++ Linux/copystdio.c
@@ -3,35 +3,29 @@
  * Copy standard input to output or files in argument.
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <string.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[]) {
+  char buf[BUFSIZ];
+  int n;
 
-int main(int argc, char *argv[])
-{
-    char buf[BUFSIZ];
-    int  n;
-
-
-    while ( ( n = read(STDIN_FILENO, buf, sizeof(buf) ) ) > 0 )
-    {
-        if ( write(STDOUT_FILENO, buf, n) != n )
-        {
-            fprintf(stderr, "write error: %s\n", strerror(errno));
-            exit(1);
-        }    
+  while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+    if (write(STDOUT_FILENO, buf, n) != n) {
+      fprintf(stderr, "write error: %s\n", strerror(errno));
+      exit(1);
     }
+  }
 
-    if ( n < 0 )
-    {
-        fprintf(stderr, "read error: %s\n", strerror(errno));
-        exit(1);
-    }
-    
-    return 0;
+  if (n < 0) {
+    fprintf(stderr, "read error: %s\n", strerror(errno));
+    exit(1);
+  }
+
+  return 0;
 }
--- Linux/listdir.c
+++ Linux/listdir.c
@@ -1,41 +1,35 @@
 /* Copyright © 2021 Chee Bin HOH. All rights reserved.
  */
 
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <libgen.h>
 #include <dirent.h>
+#include <errno.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *programName = NULL;
 
-int main(int argc, char *argv[])
-{
-    DIR           *dp;
-    struct dirent *dirp;
+int main(int argc, char *argv[]) {
+  DIR *dp;
+  struct dirent *dirp;
 
+  programName = basename(argv[0]);
 
-    programName = basename(argv[0]);
-    
-    if ( argc != 2 )
-    {
-        fprintf(stderr, "Usage: %s directory\n", programName);
-        exit(1);
-    }
- 
-    if ( ( dp = opendir(argv[1] ) ) == NULL )
-    {
-        fprintf(stderr, "%s: %s\n", programName,
-                strerror(errno));
-        exit(2);
-    }
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s directory\n", programName);
+    exit(1);
+  }
 
-    while ( ( dirp = readdir(dp) ) != NULL )
-        printf("%s\n", dirp->d_name);
+  if ((dp = opendir(argv[1])) == NULL) {
+    fprintf(stderr, "%s: %s\n", programName, strerror(errno));
+    exit(2);
+  }
 
-    closedir(dp);
+  while ((dirp = readdir(dp)) != NULL)
+    printf("%s\n", dirp->d_name);
 
-    return 0;
+  closedir(dp);
+
+  return 0;
 }
-
