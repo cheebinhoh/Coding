e3a6cd37297457e1b1e03939b6d4d25897c5538a
hohcheebin <hohcheebin@gmail.com>
2023-03-06 14:27:07 -0500

    no comment

--- C++/singleton-design-pattern.cpp
+++ C++/singleton-design-pattern.cpp
@@ -7,10 +7,43 @@
 #include <memory>
 
 class Singleton {
+public:
+  static Singleton *getInstance() {
+
+    if (!m_instance) {
+      std::cout << "No instance, create one\n";
+
+      std::unique_ptr<Singleton> new_instance{new Singleton};
+      m_instance = std::move(new_instance);
+    } else {
+      std::cout << "Instance exist, return it\n";
+    }
+
+    return m_instance.get();
+  }
+
+  ~Singleton() { std::cout << "Deinitialize Singleton object\n"; }
+
+protected:
+  Singleton() {}
+
 private:
   static std::unique_ptr<Singleton> m_instance;
 };
 
 std::unique_ptr<Singleton> Singleton::m_instance{};
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  std::cout << "Get singleton instance\n";
+  Singleton *inst = Singleton::getInstance();
+
+  {
+    std::cout << "Get singleton instance again\n";
+    Singleton *nested_inst = Singleton::getInstance();
+
+    std::cout << "Is same instance = " << std::boolalpha
+              << (nested_inst == inst) << "\n";
+  }
+
+  return 0;
+}
