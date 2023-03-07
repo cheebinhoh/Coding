943f02cce0b8e306682607f6c0bb4a587d0f2680
hohcheebin <hohcheebin@gmail.com>
2023-03-07 09:52:31 -0500

    no comment

---
+++ C++/std-array.cpp
@@ -0,0 +1,9 @@
+/**
+ * Copyright © 2023 Chee Bin HOH. All rights reserved.
+ *
+ */
+
+#include <array>
+#include <iostream>
+
+int main(int argc, char *argv[]) { return 0; }
---
+++ C++/std-pair.cpp
@@ -0,0 +1,28 @@
+/**
+ * Copyright © 2023 Chee Bin HOH. All rights reserved.
+ *
+ * Pair is a special case of Tuple.
+ */
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+int main(int argc, char *argv[]) {
+  std::pair<int, std::string> one = {1, "one"};
+  std::pair<int, std::string> two = std::make_pair(2, "two");
+  auto three = std::make_pair<int, std::string>(3, "three");
+
+  std::cout << "pair of values: (first = " << one.first << ", second = \""
+            << one.second << "\")\n";
+
+  std::cout << "pair of values: (first = " << two.first << ", second = \""
+            << two.second << "\")\n";
+
+  std::cout << "pair of values: (first = " << three.first << ", second = \""
+            << three.second << "\")\n";
+
+  std::cout << "\n";
+
+  return 0;
+}
