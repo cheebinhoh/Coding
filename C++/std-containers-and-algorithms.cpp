0733ee7c87982fb6067fb4d42a0637cd8ef5bc07
hohcheebin <hohcheebin@gmail.com>
2023-03-08 13:59:07 -0500

    no comment

--- C++/std-containers-and-algorithms.cpp
+++ C++/std-containers-and-algorithms.cpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <list>
 #include <string>
 #include <vector>
 
@@ -24,7 +25,25 @@ void printVector(const std::vector<int> &v) {
   std::cout << "\n";
 }
 
+void printList(const std::list<int> &l) {
+  int cnt;
+
+  cnt = 0;
+  for (const auto i : l) {
+    if (cnt != 0) {
+      std::cout << " ";
+    }
+
+    std::cout << i;
+    cnt++;
+  }
+
+  std::cout << "\n";
+}
+
 int main(int argc, char *argv[]) {
+  std::cout << "*** std::vector\n";
+
   std::vector<int> v1 = {1, 2, 3, 4, 5};
 
   std::cout << "vector with 5 elements: ";
@@ -53,6 +72,20 @@ int main(int argc, char *argv[]) {
   printVector(v1);
   std::cout << "\n";
 
+  // the following approach is supposedly ill-function
+  // because we erase iter prior to increment it, the
+  // reason that it does not crash is because:
+  // - std::vector is a sequential container that it has
+  //   a dynamic array allocated internally
+  // - when we delete any element, it moves the elements
+  //   after that position downward to the array, but
+  //   the iter still point to the existing N-th element
+  //   though the content is different.
+  //
+  // and it does NOT work on std::list kind of container
+  // that is pointer based, where erase an element involves
+  // delete and free the element and rechain the connection
+  // between elements via pointer.
   int cnt = 0;
   std::vector<int>::iterator iter = v1.begin();
   while (iter != v1.end()) {
@@ -87,6 +120,47 @@ int main(int argc, char *argv[]) {
   v1.resize(cnt);
   std::cout << "Remove 3 from vector, it has " << cnt << " elements left: ";
   printVector(v1);
+  std::cout << "\n";
+
+  std::cout << "*** std::list\n";
+  std::list<int> l1{1, 2, 3, 4, 5};
+  std::cout << "list of values: ";
+  printList(l1);
+
+  cnt = 0;
+  std::list<int>::iterator l_iter = l1.begin();
+  while (l_iter != l1.end()) {
+    std::list<int>::iterator tmp2del = l1.end();
+
+    if (*l_iter % 2 == 0) {
+      cnt++;
+      tmp2del = l_iter;
+    }
+
+    l_iter++;
+
+    if (tmp2del != l1.end()) {
+      l1.erase(tmp2del);
+    }
+  }
+
+  std::cout << "Number of even values is " << cnt << ", ";
+  std::cout << "delete even value from list: ";
+  printList(l1);
+  std::cout << "\n";
+
+  cnt = 0;
+  std::remove_if(l1.begin(), l1.end(), [&cnt](int i) mutable {
+    if (i != 3) {
+      cnt++;
+    }
+
+    return i == 3;
+  });
+
+  l1.resize(cnt);
+  std::cout << "Remove 3 from list, it has " << l1.size() << " elements left: ";
+  printList(l1);
   std::cout << "\n";
 
   return 0;
