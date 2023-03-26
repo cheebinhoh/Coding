139b9863344da8095922149a807da493a033066e
hohcheebin <hohcheebin@gmail.com>
2023-03-26 19:00:30 -0400

    no comment

--- C++/std-thread-with-multiple-reader-n-one-writer.cpp
+++ C++/std-thread-with-multiple-reader-n-one-writer.cpp
@@ -1,8 +1,8 @@
 /**
  * Copyright © 2023 Chee Bin HOH. All rights reserved.
- *
  */
 
+#include <array>
 #include <chrono>
 #include <condition_variable>
 #include <ctime>
@@ -15,8 +15,11 @@
 using std::chrono::system_clock;
 
 void reader_func(int seconds) {
-  int mins = seconds / 60;
-  seconds = seconds % 60;
+  long count{0};
+  int mins{seconds / 60};
+
+  seconds = {seconds % 60};
+
   std::time_t tt = system_clock::to_time_t(system_clock::now());
   struct std::tm *ptm = std::localtime(&tt);
 
@@ -27,15 +30,23 @@ void reader_func(int seconds) {
   tt = system_clock::to_time_t(system_clock::now());
   while (tt < next_tt) {
     std::this_thread::yield();
-
+    count++;
     tt = system_clock::to_time_t(system_clock::now());
   }
+
+  std::cout << std::this_thread::get_id() << ", count = " << count << "\n";
 }
 
 int main(int argc, char *argv[]) {
-  reader_func(5);
+  std::array<std::thread, 5> readers;
 
-  std::cout << "End\n";
+  for (int i = 0; i < readers.size(); i++) {
+    readers[i] = std::thread(reader_func, 5);
+  }
+
+  for (auto &th : readers) {
+    th.join();
+  }
 
   return 0;
 }
