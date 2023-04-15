9a706ac9cb46a886768b2521feb6de24f0c2cc49
hohcheebin <hohcheebin@gmail.com>
2023-04-14 20:24:45 -0400

    no comment

--- C++/ring-bufer-for-multiple-writers-n-one-reader.cpp
+++ C++/ring-bufer-for-multiple-writers-n-one-reader.cpp
@@ -30,6 +30,7 @@
 #include <map>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 using std::chrono::system_clock;
 
@@ -39,7 +40,11 @@ struct ringbuffer_t {
   std::condition_variable cv{};
   std::mutex mutex{};
 
-  std::array<long, 500> buffer{};
+  std::vector<int> buffer{std::vector<int>(50, 0)};
+  std::size_t size{buffer.size()};
+  std::size_t stepup_size{20};
+  std::size_t max_size{1000};
+
   int r_index{0};
   int w_index{0};
 
@@ -66,18 +71,15 @@ void reader_fn(struct ringbuffer_t *ringbuffer, int seconds) {
         ringbuffer->cv.wait(lck);
       }
 
-      int next_r_index = (ringbuffer->r_index + 1) % ringbuffer->buffer.size();
       while (ringbuffer->r_index == ringbuffer->w_index) {
-        auto cv_ret = ringbuffer->r_cv.wait_for(
-            lck, std::chrono::seconds(std::max(1L, next_tt - tt)));
+        auto cv_ret = ringbuffer->r_cv.wait_for(lck, std::chrono::seconds(10));
         if (std::cv_status::timeout == cv_ret) {
           goto end;
         }
       }
 
       num = ringbuffer->buffer[ringbuffer->r_index];
-      ringbuffer->r_index =
-          (ringbuffer->r_index + 1) % ringbuffer->buffer.size();
+      ringbuffer->r_index = (ringbuffer->r_index + 1) % ringbuffer->size;
 
       ringbuffer->w_cv.notify_all();
     }
@@ -90,8 +92,7 @@ void reader_fn(struct ringbuffer_t *ringbuffer, int seconds) {
 
     count++;
     tt = system_clock::to_time_t(system_clock::now());
-
-    std::this_thread::yield();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
 end:
@@ -125,13 +126,42 @@ void writer_fn(struct ringbuffer_t *ringbuffer, long initial_value,
         ringbuffer->cv.wait(lck);
       }
 
-      int next_w_index = (ringbuffer->w_index + 1) % ringbuffer->buffer.size();
+      int next_w_index = (ringbuffer->w_index + 1) % ringbuffer->size;
 
       while (next_w_index == ringbuffer->r_index) {
-        auto cv_ret = ringbuffer->w_cv.wait_for(
-            lck, std::chrono::seconds(std::max(1L, next_tt - tt)));
-        if (std::cv_status::timeout == cv_ret) {
-          goto end;
+        if (ringbuffer->size >= ringbuffer->max_size) {
+          std::cout << "pause w_thread " << std::this_thread::get_id() << "\n";
+
+          auto cv_ret = ringbuffer->w_cv.wait_for(
+              lck, std::chrono::seconds(std::max(1L, next_tt - tt)));
+          if (std::cv_status::timeout == cv_ret) {
+            goto end;
+          }
+        } else {
+          // this can be expensive if the reader thread is too slow, example is
+          // that we have a ringbuffer of 50, and reader next read is at index
+          // 4, and this writer thread next write is 4, and we have to move 4 to
+          // 49 toward to newly added buffer at the end.
+
+          std::size_t new_size{
+              std::min(ringbuffer->max_size,
+                       ringbuffer->size + ringbuffer->stepup_size)};
+
+          std::cout << "writer thread " << std::this_thread::get_id()
+                    << " extends ringbuffer size from " << ringbuffer->size
+                    << " to " << new_size << "\n";
+          ringbuffer->buffer.resize(new_size, 0);
+          int n_to_move = ringbuffer->size - ringbuffer->r_index;
+
+          // important to move from backward in case that the newly added nr of
+          // elements is smaller than n_to_move.
+          for (int i = 0; i < n_to_move; i++) {
+            ringbuffer->buffer[new_size - 1 - i] =
+                ringbuffer->buffer[ringbuffer->size - 1 - i];
+          }
+
+          ringbuffer->r_index = new_size - n_to_move;
+          ringbuffer->size = new_size;
         }
       }
 
@@ -142,7 +172,7 @@ void writer_fn(struct ringbuffer_t *ringbuffer, long initial_value,
     }
 
     initial_value++;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
     tt = system_clock::to_time_t(system_clock::now());
   }
 
@@ -165,7 +195,7 @@ int main(int argc, char *argv[]) {
 
   std::thread w1(writer_fn, &ringbuffer, 1, 60);
   std::thread w2(writer_fn, &ringbuffer, 1, 60);
-  std::thread w3(reader_fn, &ringbuffer, 80);
+  std::thread w3(reader_fn, &ringbuffer, 240);
 
   {
     std::unique_lock<std::mutex> lck(ringbuffer.mutex);
