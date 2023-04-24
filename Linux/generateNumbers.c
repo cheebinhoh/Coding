3009f3b8956795c51da1b4c592a7c28c51960a30
hohcheebin <hohcheebin@gmail.com>
2023-04-23 20:41:39 -0400

    no comment

--- Linux/generateNumbers.c
+++ Linux/generateNumbers.c
@@ -4,11 +4,25 @@
  * 20 seconds.
  */
 
+#include <assert.h>
+#include <libgen.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+
+static char *programName = NULL;
+
+void displayUsage(void) {
+  printf("%s: [-s seconds]\n", programName);
+  printf("Generate timestamp and random numbers for seconds\n");
+  printf("\n");
+  printf("options:\n");
+  printf("  -s seconds  seconds must be 1 or more, default is 20\n");
+}
 
 int main(int argc, char *argv[]) {
-  int cnt;
+  int opt;
   int seconds = 20;
   struct timespec now;
   struct timespec target;
@@ -16,7 +30,30 @@ int main(int argc, char *argv[]) {
   time_t now_time;
   struct tm *lctime;
 
-  cnt = 0;
+  programName = basename(argv[0]);
+
+  // parse command line arguments
+  while ((opt = getopt(argc, argv, "s:h")) != -1) {
+    switch (opt) {
+    case 's':
+      seconds = atoi(optarg);
+      if (seconds <= 0) {
+        displayUsage();
+        exit(1);
+      }
+      break;
+
+    case '?':
+      displayUsage();
+      exit(1);
+      break;
+
+    default:
+      assert("unhandled option");
+      break;
+    }
+  }
+
   prev.tv_sec = prev.tv_nsec = 0;
   clock_gettime(CLOCK_REALTIME, &now);
   target = now;
@@ -32,11 +69,10 @@ int main(int argc, char *argv[]) {
       char strtime[1000] = "\n";
 
       now_time = now.tv_sec;
-      cnt++;
 
       lctime = localtime(&now_time);
       strftime(strtime, sizeof(strtime), "%Y-%m-%d %I:%M:%S", lctime);
-      printf("%d: %s %ld.%ld\n", cnt, strtime, now.tv_sec, now.tv_nsec);
+      printf("%s.%-10ld%c%ld\n", strtime, now.tv_nsec, '\t', random());
       fflush(stdout);
 
       prev = now;
--- Linux/readGeneratedNumbers.c
+++ Linux/readGeneratedNumbers.c
@@ -3,12 +3,52 @@
  * This program just read standard input and write to standard output.
  */
 
+#include <assert.h>
+#include <libgen.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static char *programName = NULL;
+
+void displayUsage(void) {
+  printf("%s: [-f file]\n", programName);
+  printf("Read timestamp and random numbers\n");
+  printf("\n");
+  printf("options:\n");
+  printf("  -f file  file to read the content from, default is stdin\n");
+}
 
 int main(int argc, char *argv[]) {
+  int opt;
   char s[1024];
+  FILE *fptr = stdin;
 
-  while (NULL != fgets(s, sizeof(s), stdin)) {
+  programName = basename(argv[0]);
+
+  // parse command line arguments
+  while ((opt = getopt(argc, argv, "f:h")) != -1) {
+    switch (opt) {
+    case 'f':
+      fptr = fopen(optarg, "r");
+      if (NULL == fptr) {
+        perror(programName);
+        exit(1);
+      }
+      break;
+
+    case '?':
+      displayUsage();
+      exit(1);
+      break;
+
+    default:
+      assert("unhandled option");
+      break;
+    }
+  }
+
+  while (NULL != fgets(s, sizeof(s), fptr)) {
     fputs(s, stdout);
   }
 
