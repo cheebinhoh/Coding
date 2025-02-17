111ee05013be40df1961ebaa1bf07a13471cc23b
hohcheebin <cheebinhoh@gmail.com>
2025-02-17 14:43:49 -0500

    no comment

--- Hal/hal-singleton.hpp
+++ Hal/hal-singleton.hpp
@@ -1,13 +1,17 @@
+/**
+ * Copyright © 2024 - 2025 Chee Bin HOH. All rights reserved.
+ *
+ * This class implements a singleton class that subclass can inheritted
+ * from and used to create a singleton instance with arguments specific
+ * to the subclass constructor.
+ */
 
 #ifndef HAL_SINGLETON_HPP_HAVE_SEEN
 
 #define HAL_SINGLETON_HPP_HAVE_SEEN
 
 #include <memory>
-#include <mutex>
 #include <utility>
-
-#include <iostream>
 
 class Hal_Singleton {
 public:
---
+++ Hal/hal.cpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2025 Chee Bin HOH. All rights reserved.
+ *
+ * This is the hal daemon that runs as after a system bootup,
+ * and receive and read, write and process message from network
+ * of distributed hal nodes.
+ */
+
+#include "hal.hpp"
+
+#include <iostream>
+
+int main(int argc, char *argv[]) { return 0; }
