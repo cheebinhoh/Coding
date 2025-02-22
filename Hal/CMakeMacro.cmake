e3e96258c3afe3df3bdd4e2248c0f80e7457484d
hohcheebin <cheebinhoh@gmail.com>
2025-02-22 12:33:26 -0500

    no comment

--- Hal/CMakeMacro.cmake
+++ Hal/CMakeMacro.cmake
@@ -14,3 +14,16 @@ macro(ADD_TEST_EXECUTABLE ...)
     add_test(NAME ${arg} COMMAND ${arg})
   endforeach()
 endmacro()
+
+macro(GENERATE_PROTOBUF ...)
+  foreach(arg ${ARGN})
+    target_include_directories(${ARGV0} PRIVATE
+                               ${CMAKE_CURRENT_BINARY_DIR})
+
+    protobuf_generate(TARGET ${ARGV0}
+                      PROTOS ${CMAKE_CURRENT_SOURCE_DIR}/proto/${arg}
+                      LANGUAGE cpp
+                      OUT_VAR PROTOBUF_GENERATED_FILES
+                      PROTOC_OUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
+  endforeach()
+endmacro()
