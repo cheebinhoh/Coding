488b237250acbcd533191bf90831b79c6ff8078c
hohcheebin <cheebinhoh@gmail.com>
2025-03-02 18:40:47 -0500

    no comment

--- Hal/hal-dmesgnet.hpp
+++ Hal/hal-dmesgnet.hpp
@@ -1,7 +1,9 @@
 /**
  * Copyright © 2025 Chee Bin HOH. All rights reserved.
  *
- * The Hal_DMesgNet stands for HAL Distributed Messaging over network.
+ * The Hal_DMesgNet stands for HAL Distributed Messaging over network,
+ * it acts as short of agent to integrating the Hal_DMesg within a
+ * host over Hal_Io with another Hal_DMesg across network or IPC.
  */
 
 #ifndef HAL_DMESGNET_HPP_HAVE_SEEN
@@ -20,14 +22,24 @@ namespace Hal {
 
 class Hal_DMesgNet : public Hal_DMesg {
 public:
-  Hal_DMesgNet(std::string_view name) : Hal_DMesg{name}, m_name{name} {
+  Hal_DMesgNet(std::string_view name,
+               std::shared_ptr<Hal_Io<std::string>> output = nullptr)
+      : Hal_DMesg{name}, m_name{name}, m_outputHandler{output} {
     m_subscriptHandler = Hal_DMesg::openHandler(
         m_name,
         [this](const Hal::DMesgPb &dmesgPb) {
           return dmesgPb.source() != this->m_name;
         },
-        [this](Hal::DMesgPb dmesgPb) {
-          // FIXME: send output to the network!
+        [this](Hal::DMesgPb dmesgPb) mutable {
+          if (m_outputHandler) {
+            std::string serialized_string{};
+
+            dmesgPb.set_source(this->m_name); // so that we can skip it
+                                              // if we read it back too.
+            dmesgPb.SerializeToString(&serialized_string);
+
+            m_outputHandler->write(serialized_string);
+          }
         });
   }
 
@@ -44,6 +56,8 @@ public:
 
 private:
   std::string m_name{};
+  std::shared_ptr<Hal_Io<std::string>> m_outputHandler{};
+
   std::shared_ptr<Hal_DMesgHandler> m_subscriptHandler{};
 };
 
--- Hal/test/hal-test-dmesgnet.cpp
+++ Hal/test/hal-test-dmesgnet.cpp
@@ -3,10 +3,12 @@
  */
 
 #include "hal-dmesgnet.hpp"
+#include "hal-socket.hpp"
 
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <thread>
 
@@ -14,9 +16,25 @@ int main(int argc, char *argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
 
-  Hal::Hal_DMesgNet dmesgnet1{"dmesg1"};
-  Hal::Hal_DMesgNet dmesgnet2{"dmesg2"};
- 
+  std::shared_ptr<Hal::Hal_Io<std::string>> readSocket1 = std::make_shared<Hal::Hal_Socket>("127.0.0.1", 5000);
+  std::shared_ptr<Hal::Hal_Io<std::string>> writeSocket1 = std::make_shared<Hal::Hal_Socket>("127.0.0.1",
+                                                                                            5000,
+                                                                                            true);
+
+  Hal::Hal_DMesgNet dmesgnet1{"dmesg1", writeSocket1};
+
+  Hal::DMesgPb dmesgPbRead{};
+  Hal::Hal_Proc proc2{"dmesg2", [readSocket1, &dmesgPbRead](){
+    auto data = readSocket1->read();
+    if (data) {
+      dmesgPbRead.ParseFromString(*data);
+    }
+  }};
+
+  proc2.exec();
+  Hal::Hal_Proc::yield();
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+
   auto dmesgHandler = dmesgnet1.openHandler("writeHandler", nullptr, nullptr);
   EXPECT_TRUE(dmesgHandler);
  
@@ -31,6 +49,11 @@ int main(int argc, char *argv[])
   dmesgHandler->write(dmesgPb);
 
   std::this_thread::sleep_for(std::chrono::seconds(5));
+
+  std::string source = dmesgPbRead.source();
+  EXPECT_TRUE(dmesgPbRead.type() == dmesgPb.type());
+  EXPECT_TRUE(dmesgPbRead.source() == "dmesg1");
+  EXPECT_TRUE(dmesgPbRead.body().message() == dmesgPb.body().message());
  
   return RUN_ALL_TESTS();
 }
