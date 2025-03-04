6396d532edea4511d51bdf27ed751bad09e4a5d5
hohcheebin <cheebinhoh@gmail.com>
2025-03-04 14:32:56 -0500

    no comment

--- Hal/hal-dmesg-pb-util.hpp
+++ Hal/hal-dmesg-pb-util.hpp
@@ -18,16 +18,26 @@
   struct timeval pb##_tv;                                                      \
   gettimeofday(&(pb##_tv), NULL);                                              \
   (pb).mutable_timestamp()->set_seconds(pb##_tv.tv_sec);                       \
-  (pb).mutable_timestamp()->set_nanos(pb##_tv.tv_usec * 1000);
+  (pb).mutable_timestamp()->set_nanos(pb##_tv.tv_usec * 1000)
 
 #define DMESG_PB_SET_IDENTIFIER(pb, val) ((pb).set_identifier((val)))
+
 #define DMESG_PB_SET_RUNNINGCOUNTER(pb, val) ((pb).set_runningcounter((val)))
+
 #define DMESG_PB_SET_SOURCEIDENTIFIER(pb, val)                                 \
   ((pb).set_sourceidentifier((val)))
+
 #define DMESG_PB_SET_TYPE(pb, val) ((pb).set_type((val)))
 
 #define DMESG_PB_MSG_SET_MESSAGE(pb, val)                                      \
   assert((pb).type() == Hal::DMesgTypePb::message);                            \
   ((pb).mutable_body()->set_message((val)))
+
+#define DMESG_PB_SYS_INIT_TIMESTAMP(pb)                                                     \
+  assert((pb).type() == Hal::DMesgTypePb::sys);                                             \
+  struct timeval pb##sys_tv;                                                                \
+  gettimeofday(&(pb##sys_tv), NULL);                                                        \
+  ((pb).mutable_body()->mutable_sys()->mutable_timestamp()->set_seconds(pb##sys_tv.tv_sec);       \
+  ((pb).mutable_body()->mutable_sys()->mutable_timestamp()->set_nanos(pb##sys_tv.tv_usec * 1000)
 
 #endif /* HAL_DMESG_PB_UTIL_HPP_HAVE_SEEN */
--- Hal/test/hal-test-dmesg-pb-util.cpp
+++ Hal/test/hal-test-dmesg-pb-util.cpp
@@ -28,6 +28,19 @@ int main(int argc, char *argv[])
   EXPECT_TRUE(dmesgPb.type() == Hal::DMesgTypePb::message);
   EXPECT_TRUE(dmesgPb.body().message() == "hello dmesg");
 
+  DMESG_PB_INIT(dmesgPb2);
+  DMESG_PB_SET_IDENTIFIER(dmesgPb2, "id2");
+  DMESG_PB_SET_RUNNINGCOUNTER(dmesgPb2, 100);
+  DMESG_PB_SET_SOURCEIDENTIFIER(dmesgPb2, "node2");
+  DMESG_PB_SET_TYPE(dmesgPb2, Hal::DMesgTypePb::sys);
+
+  EXPECT_TRUE(dmesgPb2.timestamp().seconds() != 0);
+  EXPECT_TRUE(dmesgPb2.timestamp().nanos() != 0);
+  EXPECT_TRUE(dmesgPb2.identifier() == "id2");
+  EXPECT_TRUE(dmesgPb2.runningcounter() == 100);
+  EXPECT_TRUE(dmesgPb2.sourceidentifier() == "node2");
+  EXPECT_TRUE(dmesgPb2.type() == Hal::DMesgTypePb::sys);
+
   return RUN_ALL_TESTS();
 }
 
