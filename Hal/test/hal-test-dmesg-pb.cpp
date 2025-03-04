5b2236df9ab12e235f38b52b34cd696991af43cf
hohcheebin <cheebinhoh@gmail.com>
2025-03-03 20:34:33 -0500

    no comment

--- Hal/test/hal-test-dmesg-pb.cpp
+++ Hal/test/hal-test-dmesg-pb.cpp
@@ -41,9 +41,33 @@ int main(int argc, char *argv[])
   dmesgSysBodySysPb->mutable_timestamp()->set_seconds(tv.tv_sec);
   dmesgSysBodySysPb->mutable_timestamp()->set_nanos(tv.tv_usec * 1000);
 
+  Hal::DMesgSysNodePb *self = dmesgSysBodySysPb->mutable_self();
+  self->set_state(Hal::DMesgStatePb::MasterPending);
+  self->set_identifier("id2");
+  self->mutable_initializedtimestamp()->set_seconds(tv.tv_sec);
+  self->mutable_initializedtimestamp()->set_nanos(tv.tv_usec * 1000);
+
+  auto neightborNode1 = dmesgSysBodySysPb->add_nodelist();
+  neightborNode1->set_state(Hal::DMesgStatePb::Ready);
+  neightborNode1->set_identifier("id3");
+  neightborNode1->set_masteridentifier("id3");
+  neightborNode1->mutable_initializedtimestamp()->set_seconds(tv.tv_sec);
+  neightborNode1->mutable_initializedtimestamp()->set_nanos(tv.tv_usec * 1000);
+
+  auto neightborNode2 = dmesgSysBodySysPb->add_nodelist();
+  neightborNode2->set_state(Hal::DMesgStatePb::Ready);
+  neightborNode2->set_identifier("id4");
+  neightborNode2->set_masteridentifier("id3");
+  neightborNode2->mutable_initializedtimestamp()->set_seconds(tv.tv_sec);
+  neightborNode1->mutable_initializedtimestamp()->set_nanos(tv.tv_usec * 1000);
+
   EXPECT_TRUE(dmesgSysPb.type() == Hal::DMesgTypePb::sys);
   EXPECT_TRUE(dmesgSysPb.body().has_sys());
   EXPECT_TRUE(!dmesgSysPb.body().has_message());
+  EXPECT_TRUE(dmesgSysPb.body().sys().nodelist().size() == 2);
+  EXPECT_TRUE(dmesgSysPb.body().sys().nodelist().Get(0).identifier() == "id3");
+  EXPECT_TRUE(dmesgSysPb.body().sys().nodelist().Get(0).masteridentifier() == "id3");
+  EXPECT_TRUE(dmesgSysPb.body().sys().nodelist().Get(0).state() == Hal::DMesgStatePb::Ready);
 
   return RUN_ALL_TESTS();
 }
