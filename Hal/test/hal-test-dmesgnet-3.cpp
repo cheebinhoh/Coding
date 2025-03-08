7df54cc660d618e7b03a3d59f3ee12b4a8778318
hohcheebin <cheebinhoh@gmail.com>
2025-03-07 22:38:53 -0500

    no comment

--- Hal/test/hal-test-dmesgnet-3.cpp
+++ Hal/test/hal-test-dmesgnet-3.cpp
@@ -20,10 +20,53 @@ int main(int argc, char *argv[])
   std::shared_ptr<Hal::Hal_Io<std::string>> writeSocket1 = std::make_shared<Hal::Hal_Socket>("127.0.0.1",
                                                                                             5000,
                                                                                             true);
+
+  Hal::DMesgPb sysPb_3{};
   Hal::Hal_DMesgNet dmesgnet1{"dmesg-3", writeSocket1, readSocket1};
 
-  std::this_thread::sleep_for(std::chrono::seconds(10));
+  auto listenHandler3 = dmesgnet1.openHandler("dmesg-3-listen", true,
+                                              nullptr,
+                                              [&sysPb_3](Hal::DMesgPb data) mutable {
+                                                if (data.type() == Hal::DMesgTypePb::sys) {
+                                                  sysPb_3 = data;
+                                                }
+                                              });
+
+  Hal::DMesgPb sysPb_4{};
+
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  Hal::Hal_Proc dmesg_4_Proc{"dmesg_4_Proc",
+                             [&sysPb_4]() mutable {
+                               std::shared_ptr<Hal::Hal_Io<std::string>> readSocket1 = 
+                                 std::make_shared<Hal::Hal_Socket>("127.0.0.1", 5000);
+                               std::shared_ptr<Hal::Hal_Io<std::string>> writeSocket1 =
+                                 std::make_shared<Hal::Hal_Socket>("127.0.0.1",
+                                                                   5001,
+                                                                   true);
+
+                               Hal::Hal_DMesgNet dmesgnet1{"dmesg-4", writeSocket1, readSocket1};
+                               auto listenHandler4 = dmesgnet1.openHandler("dmesg-4-listen", true,
+                                                                           nullptr,
+                                                                           [&sysPb_4](Hal::DMesgPb data) mutable {
+                                                                             if (data.type() == Hal::DMesgTypePb::sys) {
+                                                                               sysPb_4 = data;
+                                                                             }
+                                                                           });
+
+                               std::this_thread::sleep_for(std::chrono::seconds(7));
+                               dmesgnet1.waitForEmpty();
+                             }};
+
+  dmesg_4_Proc.exec();
+
+  std::this_thread::sleep_for(std::chrono::seconds(7));
   dmesgnet1.waitForEmpty();
+  dmesg_4_Proc.wait();
+
+  EXPECT_TRUE(sysPb_3.body().sys().self().identifier() == "dmesg-3");
+  EXPECT_TRUE(sysPb_3.body().sys().self().masteridentifier() == "dmesg-3");
+  EXPECT_TRUE(sysPb_4.body().sys().self().identifier() == "dmesg-4");
+  EXPECT_TRUE(sysPb_4.body().sys().self().masteridentifier() == "dmesg-3");
 
   return RUN_ALL_TESTS();
 }
