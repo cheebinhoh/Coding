31750744ab81fb2c1867e38e3cafe9ff0cdfbe94
Chee Bin Hoh <cheebinhoh@pop-os.mynetworksettings.com>
2026-05-09 13:40:07 -0400

    hello glib again

---
+++ gnome/glib/hello-glib.c
@@ -0,0 +1,14 @@
+/**
+ * Copyright © 2026 Chee Bin HOH. All rights reserved.
+ *
+ * @file hello-glib.c
+ * @brief Hello program from GLib
+ */
+
+#include <glib.h>
+
+int main() {
+  g_print("Hello from GLib!\n");
+
+  return 0;
+}
